5168fccbc013678a9b9008a31dbdb41ece8e8c53
Nadalbdh <nd.lbdh@gmail.com>
2023-11-01 22:14:59 +0100

    create movie, review, user

---
+++ src/main/java/com/slytherin/movies/User.java
@@ -0,0 +1,18 @@
+package com.slytherin.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+}
